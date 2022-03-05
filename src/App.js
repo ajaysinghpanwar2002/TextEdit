bba09ecb443ee670ce26eb0d4f051959d4004705
Ajay <ajaysingh07032003@gmail.com>
2022-03-05 17:00:05 +0530

    Simple Text editor
    I created this app using React and Bootstrap .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+// import About from './components/About';
+// import React, { useState } from 'react';
+import Alert from './components/Alert';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
+
 
 function App() {
+  const [Mode, setMode] = useState('dark');// whether dark mode is enabled or not 
+  const [alert, setAlert] = useState(null)
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
+
+
+  const toggleMode = () => {
+    if (Mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert(" Dark mode has been enabled", "primary");
+      document.title = 'TextEdit-Dark Mode';
+
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert(" Light mode has been enabled", "success");
+      document.title = 'TextEdit-Light Mode';
+
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="TextEdit" about="About" mode={Mode} toggleMode={toggleMode} />
+      <div className="container" my-3  >
+        <Alert alert={alert} />
+        <strong><TextForm showAlert={showAlert} heading="Enter your text" /></strong>
+      </div>
+    </>
   );
 }
+export default App
 
-export default App;
+
+
